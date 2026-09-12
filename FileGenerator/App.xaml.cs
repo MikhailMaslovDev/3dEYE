47e39d23d8bd5f187ffab1e52fae2e1471ff89bb
Mikhail Maslov <ovofem@gmail.com>
2026-09-12 12:55:38 +0200

    Add FileGenerator

---
+++ FileGenerator/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FileGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ FileGenerator/GenerationStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace FileGenerator;
+
+internal sealed record GenerationStatus(
+    long WrittenBytes,
+    long WrittenLines,
+    double BytesPerSecond,
+    long TargetBytes);
